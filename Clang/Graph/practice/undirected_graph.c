409e5e9d30fce93dbfa759cda42cc4702815dc14
Fahim Abrar Saikat <fahimabrar.saikat@gmail.com>
2021-07-14 09:31:30 +0600

    added incidence matrix & structure

--- Clang/Graph/practice/undirected_graph.c
+++ Clang/Graph/practice/undirected_graph.c
@@ -7,22 +7,33 @@
 
 #include <stdio.h>
 
+struct Edge {
+    int start;
+    int end;
+};
+
+
+void input(int arr[10][10], int vertex, struct Edge edge[100]);
 void print(int arr[10][10], int vertex);
 void assign(int arr[10][10], int vertex);
-void input(int arr[10][10], int vertex);
+/* void input(int arr[10][10], int vertex); */
 void edgeList(int arr[10][10], int vertex);
 void adjList(int arr[10][10], int vertex);
+int incidence(int arr[10][10], int vertex, int start, int end);
+void print_edge_list(struct Edge edge[], int num_edge);
+void print_incidence_matrix(int arr[10][10], int vertex, struct Edge edge[], int num_edge);
 
 int main()
 {
     int row, col;
     int graph[10][10];
     int vertex = 8;
+    struct Edge edge[100];
 
     assign(graph, 10);
     print(graph, vertex);
 
-    input(graph, 11);
+    input(graph, 11, edge);
     printf("After inserting:\n");
     print(graph, vertex);
 
@@ -31,6 +42,11 @@ int main()
 
     adjList(graph, vertex);
     print(graph, vertex);
+
+    incidence(graph, vertex, 0, 0) ?  printf("Adjacent\n") : printf("Not Adjacent\n");
+    print_edge_list(edge, 11);
+    printf("Incidence Matrix: \n");
+    print_incidence_matrix(graph, vertex, edge, 11);
 
     return 0;
 }
@@ -61,15 +77,20 @@ void print(int arr[10][10], int vertex)
 }
 
 
-void input(int arr[10][10], int edge)
+void input(int arr[10][10], int vertex, struct Edge edge[100])
 {
     int i;
     int row, col;
 
-    for (i = 0; i < edge; i++) {
+    for (i = 0; i < vertex; i++) {
         scanf("%d %d", &row, &col);
         arr[row][col] = 1;
         arr[col][row] = 1;
+
+        /* Stroring Edges */
+        edge[i].start = row;
+        edge[i].end = col;
+
     }
 }
 
@@ -102,3 +123,35 @@ void adjList(int arr[10][10], int vertex)
 }
 
 
+int incidence(int arr[10][10], int vertex, int start, int end)
+{
+    return arr[start][end];
+}
+
+
+void print_incidence_matrix(int arr[10][10], int vertex, struct Edge edge[], int num_edge)
+{
+    int node, i, j;
+
+    for (node = 0; node < vertex; node++) {
+        printf("%d: ", node);
+        for (i = 0; i < num_edge; i++) {
+            if (node == edge[i].start || node == edge[i].end) {
+                printf("1 ");
+            }
+            else {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+void print_edge_list(struct Edge edge[], int num_edge)
+{
+    int i;
+    for (i = 0; i < num_edge; i++) {
+        printf("%d %d\n", edge[i].start, edge[i].end);
+    }
+}
